c879d48d2dcdce0a4208feecfc07254672a0818f
Anthony Marcar <amarcar@walmartlabs.com>
2013-08-11 14:23:38 -0700

    jvm opts typo

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.6"]]
-  :jvm-opts ["-Xms256m" " -Xmx1024m"]
+  :jvm-opts ["-Xms256m" "-Xmx1024m"]
   :main core)
