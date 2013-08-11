2a2c28907fe55319c30ade54e4dcbec659caef9e
Onurbon <nomail@example.com>
2013-08-11 18:57:04 +0100

    adding new function progsAux2: a faster version of progsAux to enumerate programs, guessing the exact number of each operator. Unlike progAux, it also generate functions where all the operators occur (no need to subselect at the end). Note that enum/progs is still using progsAux, and not yet progsAux2

--- src/enum.clj
+++ src/enum.clj
@@ -17,45 +17,70 @@
   "minimal size of a program having the set of operators `ops`"
   (reduce + 1 (map arity ops)))
 
-(defn guessCounts [size ops]
-  "guess the number of occurence of each operators knowing size and set of operators" 
+(defn guessCountsAux [size ops upb]
+  "guess the number of occurence of each operators knowing size and set of operators
+   when upb is not specified, it is assumed that each operator must occur at least once
+   when given, upb is a mapping providing an upper bound on the number of each operator" 
   (let [o (first ops) 
         a (arity o) 
+        u (if (upb o) (upb o) size) 
         tail (disj ops (first ops))] 
    (if (empty? ops) (seq (list {})) 
-    (for [ts (range (minSize tail) (if (empty? tail) 2 (+ 1 (- size a))))
+    (for [ts (range (minSize tail) (if (empty? tail) 2 (+ 1 (- size (if (upb o) 0 a)))))
           :let [os (- size ts)]
           :let [cnt (int (Math/floor (/ os a)))]
           :when (= os (* cnt a))
-          tres (guessCounts ts tail)]
-    (merge tres {o cnt})))))
+          :when (<= cnt u)
+          tres (guessCountsAux ts tail upb)]
+    (if (= 0 cnt) tres (merge tres {o cnt}))))))
 
-(defn decr [k m] 
-  "decrement the value of key k in map m and delete the key if new value is 0"
-  (if (= 1 (m k)) (dissoc m k) (merge m {k (- (m k) 1)})))
+(defn guessCounts [size ops] 
+   "guess the number of occurence of each opperator, given a set ops" 
+   (guessCountsAux size ops {}))
 
-;; (defn progsAuxCnt
-;;  "possible programs of given knowning the number of occurence of each operator"
-;;  [size opsmap]
-;;  ;; (println "size: " size ", opsmap: " opsmap)
-;;  (if (= size 1)
-;;    [0 1 'x]
-;;    (concat
-;;     (for [o (key opsmap) :when (= 1 (arity o))
-;;           p (progsAux (- size 1) (decr o opsmap))]
-;;       (list o p))
-;;     (for [o  (key opsmap) :when (= 2 (arity o))
-;;           s  (range 1 (Math/floor (/ size 2)))
-;;           p1 (progsAux s (decr o opsmap)
-;;           p2 (progsAux (- size 1 s) ops)]
-;;       (list o p1 p2))
-;;     (for [o ops :when (= 2 (arity o))
-;;           :when (odd? size)
-;;           :let [s (/ (- size 1) 2)]
-;;           p1 (progsAux s ops)
-;;           p2 (progsAux s ops)
-;;           :when (>= (compare ( f/to-string p1) (f/to-string p2)) 0)]
-;;       (list o p1 p2)))))
+(defn guessSubCounts [size opsmap] 
+   "guess the possible counts of each operator in a sub expression" 
+   (guessCountsAux size (set (keys opsmap)) opsmap))
+
+(defn decr [k n m] 
+  "decrement the value of key k by n in map m and delete the key if new value is 0"
+  (if (= n (m k)) (dissoc m k) (merge m {k (- (m k) n)})))
+
+(defn decrMap [m1 m2] 
+  "apply decr [key value] to m2 for each key-value pair in m1 "
+  (reduce #(decr %2 (m1 %2) %1) m2 (keys m1))
+)
+
+
+(defn progsAuxCnt
+  "possible programs of given size knowning the number of occurence of each operator"
+  [size opsmap]
+  ;; (println "size: " size ", opsmap: " opsmap)
+  (if (= size 1)
+    [0 1 'x]
+    (concat
+     (for [o  (set (keys opsmap)) :when (= 1 (arity o))
+           p (progsAuxCnt (- size 1) (decr o 1 opsmap))]
+       (list o p))
+     (for [o  (set (keys opsmap)) :when (= 2 (arity o))
+           s  (range 1 (Math/floor (/ size 2)))
+           m  (guessSubCounts s (decr o 1 opsmap))
+           p1 (progsAuxCnt s m)
+           p2 (progsAuxCnt (- size 1 s) (decrMap m (decr o 1 opsmap)))]
+       (list o p1 p2))
+     (for [o  (set (keys opsmap)) :when (= 2 (arity o))
+           :when (odd? size)
+           :let [s (/ (- size 1) 2)]
+           m  (guessSubCounts s (decr o 1 opsmap))
+           p1 (progsAuxCnt s m)
+           p2 (progsAuxCnt s (decrMap m (decr o 1 opsmap)))
+           :when (>= (compare ( f/to-string p1) (f/to-string p2)) 0)]
+       (list o p1 p2)))))
+
+(defn progsAux2 [size ops]
+ "Same behaviour as progAux"
+ (reduce concat #{} (map #(progsAuxCnt size %) (guessCounts size ops)))  )
+
 
 
 (defn symbols [sexp]
